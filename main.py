dd6ad05c368febfc5845dae72660c5f222187eea
RyunK <lrk9582@gmail.com>
2026-07-05 13:53:27 +0900

    feat: main.py 추가

---
+++ main.py
@@ -0,0 +1,33 @@
+from queue import Queue
+from threading import Thread
+
+from packet_capture import PacketCapture
+from processor import PacketProcessor
+
+
+packet_queue = Queue()
+
+capture = PacketCapture(packet_queue)
+
+processor = PacketProcessor(packet_queue)
+
+
+capture_thread = Thread(
+    target=capture.start,
+    daemon=True
+)
+
+processor_thread = Thread(
+    target=processor.run,
+    daemon=True
+)
+
+
+capture_thread.start()
+
+processor_thread.start()
+
+
+capture_thread.join()
+
+processor_thread.join()
