ac5df4ea960435c46ce6ea687fb9534e76c9e5c1
RyunK, Lee <lrk222@naver.com>
2026-07-21 09:41:53 +0900

    feat: 자동차단 dict 구성

--- engine/auto_block.py
+++ engine/auto_block.py
@@ -1,7 +1,12 @@
 
 class AutoBlock:
     def __init__(self):
-        pass
+        self.th_dict = {
+            "low": 0.1,
+            "medium": 7,
+            "high": 9,
+            "critical": 10.1,
+        }
 
     # 어디서부터 막을지 가져오기
     # 점수가 데이터 이상이면 iptables 차단하고 블랙리스트에 넣기
@@ -10,3 +15,14 @@ class AutoBlock:
         """
         어디서부터 자동차단할지 가져오는 함수
         """
+        # select 해서 가져오기
+        # 
+        return "Medium"
+
+    def auto_block(self, score, src_ip):
+        """
+        자동 차단
+        """
+        threshold = self.get_threshold()
+
+        
